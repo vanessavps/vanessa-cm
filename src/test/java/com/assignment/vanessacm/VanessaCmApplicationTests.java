3d791eaab923f55614d05017958a8e4d6c5b2249
vanessavps <vanessa.vps29@gmail.com>
2022-04-18 13:32:55 +1200

    First commit

---
+++ src/test/java/com/assignment/vanessacm/VanessaCmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.vanessacm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanessaCmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
