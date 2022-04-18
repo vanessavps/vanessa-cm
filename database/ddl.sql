0683192a5c742f71f5309f01a6b5ecb061381837
vanessavps <vanessa.vps29@gmail.com>
2022-04-18 13:52:50 +1200

    Adding DDL script

---
+++ database/ddl.sql
@@ -0,0 +1,48 @@
+#Create DB
+CREATE DATABASE IF NOT EXISTS cm_assignment;
+USE cm_assignment;
+
+#Create tables
+CREATE TABLE IF NOT EXISTS contact
+(
+    id    INT AUTO_INCREMENT PRIMARY KEY,
+    name  VARCHAR(80) NOT NULL,
+    email VARCHAR(255) DEFAULT NULL,
+    phone VARCHAR(20)  DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS address
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    address1 VARCHAR(120) DEFAULT NULL,
+    address2 VARCHAR(120) DEFAULT NULL,
+    suburb   VARCHAR(100) DEFAULT NULL,
+    city     VARCHAR(2)   DEFAULT NULL,
+    postcode VARCHAR(16)  DEFAULT NULL,
+    country  VARCHAR(2)   DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS organisation
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(80) DEFAULT NULL,
+    url        VARCHAR(50) DEFAULT NULL,
+    address_id INT,
+    FOREIGN KEY (address_id)
+        REFERENCES address (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS organisation_contacts
+(
+    organisation_id INT NOT NULL,
+    contact_id      INT NOT NULL,
+    PRIMARY KEY (organisation_id, contact_id),
+    CONSTRAINT `organisation_contact_fk`
+        FOREIGN KEY (organisation_id)
+            REFERENCES organisation (id)
+            ON DELETE CASCADE,
+    FOREIGN KEY (contact_id)
+        REFERENCES contact (id)
+        ON DELETE CASCADE
+);
